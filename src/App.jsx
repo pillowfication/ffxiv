df3a363119555c8a2b62ccb0d185d33ed6b6ffa2
Markus Tran <thebluepillow@gmail.com>
2020-06-27 16:27:59 -0700

    add a page

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,63 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import cn from 'classnames'
 
-import styles from './App.scss'
+import MiniCactpot from './mini-cactpot/MiniCactpot.jsx'
+import zf from './foundation.scss'
+import './App.scss'
 
-class TopBar extends Component {
+function kebabCase (str) {
+  return str
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(x => x.toLowerCase())
+    .join('-')
+}
+
+const pages = [{
+  title: 'Mini Cactpot',
+  component: MiniCactpot
+}]
+
+class App extends Component {
   render () {
     return (
-      <div className={styles.topBar}>
-        <div className={styles.container}>
-          foofoo
-        </div>
-      </div>
+      <Router>
+        <header className={zf.topBar}>
+          <div className={cn(zf.gridContainer, zf.cell)}>
+            <div className={zf.topBarLeft}>
+              <Link to='/'>FFXIV</Link>
+            </div>
+          </div>
+        </header>
+        <main className={zf.gridContainer}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            {pages.map(page =>
+              <Route
+                key={page.title}
+                path={'/' + kebabCase(page.title)}
+                component={page.component}
+              />)}
+          </Switch>
+        </main>
+      </Router>
     )
   }
 }
 
 class Home extends Component {
   render () {
-    return <div>Hello world</div>
-  }
-}
-
-class App extends Component {
-  render () {
     return (
       <>
-        <TopBar />
-        <div className={styles.container}>
-          <Router>
-            <Switch>
-              <Route exact path='/' component={Home} />
-            </Switch>
-          </Router>
-        </div>
+        <h1>Home</h1>
+        <ul>
+          {pages.map(page =>
+            <li key={page.title}>
+              <Link to={'/' + kebabCase(page.title)}>{page.title}</Link>
+            </li>
+          )}
+        </ul>
       </>
     )
   }
---
+++ src/mini-cactpot/compute-starting-positions.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const { getSuggestion } = require('./solve')
+
+const CACHE_LOCATION = path.resolve(__dirname, './cache.json')
+
+const cache = []
+for (let i = 0; i < 9; ++i) {
+  const grid = Array(9).fill(null)
+  for (let digit = 1; digit <= 9; ++digit) {
+    grid[i] = digit
+    const suggestion = getSuggestion(grid, true)
+    cache.push({
+      v: suggestion.maxCellEV,
+      c: suggestion.maxCellLocations.length > 1 ? suggestion.maxCellLocations : suggestion.maxCellLocations[0]
+    })
+  }
+}
+
+fs.writeFileSync(CACHE_LOCATION, JSON.stringify(cache))
