97126fc443866d38d7d49986afa0e6769c26731f
Markus Tran <thebluepillow@gmail.com>
2020-07-01 00:59:56 -0700

    add one timer

---
+++ src/timers/FFXIVTimer.js
@@ -0,0 +1,45 @@
+import moment from 'moment'
+
+class FFXIVTimer {
+  constructor () {
+    this.lastCompletedAt = null
+  }
+
+  complete () {
+    this.lastCompletedAt = moment.utc()
+  }
+
+  toString () {
+    return JSON.stringify({ lastCompletedAt: this.lastCompletedAt })
+  }
+}
+
+export class GCTimer extends FFXIVTimer {
+  getResetInterval () {
+    const now = moment.utc()
+    const resetMark = moment.utc().set(this.constructor.resetTime)
+
+    if (now.diff(resetMark) < 0) {
+      return [resetMark.clone().subtract(1, 'day'), resetMark]
+    } else {
+      return [resetMark, resetMark.clone().add(1, 'day')]
+    }
+  }
+
+  isAvailable () {
+    return this.lastCompletedAt === null || this.lastCompletedAt < this.getResetInterval()[0]
+  }
+
+  getResetInfo () {
+    const resetInterval = this.getResetInterval()
+    return {
+      lastReset: resetInterval[0],
+      nextReset: resetInterval[1],
+      timeUntilNextReset: moment.duration(resetInterval[1].diff(moment.utc()))
+    }
+  }
+}
+GCTimer.timerName = 'Grand Company Missions'
+GCTimer.resetTime = { h: 20, m: 0, s: 0, ms: 0 } // GC Turn-ins reset every day at 8pm UTC
+
+export default FFXIVTimer
--- src/timers/Timers.jsx
+++ src/timers/Timers.jsx
@@ -1,13 +1,118 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import cn from 'classnames'
+import propTypes from 'prop-types'
+import {
+  GCTimer
+} from './FFXIVTimer.js'
 
+import zf from '../foundation.scss'
 import styles from './Timers.scss'
 
+const UTC = moment().utcOffset()
+const TIMER_TYPES = [
+  GCTimer
+]
+
+function toUTCString (utc) {
+  return `UTC${utc >= 0 ? '+' : '−'}${paddedZero(Math.abs(utc / 60 | 0))}:${paddedZero(Math.abs(utc) % 60)}`
+}
+
+function paddedZero (n) {
+  return n > 9 ? n : '0' + n
+}
+
 class Timers extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      timers: window.localStorage.getItem('timers') || []
+    }
+
+    this.handleOnAddTimer = this.handleOnAddTimer.bind(this)
+  }
+
+  handleOnAddTimer (timer) {
+    const newTimers = this.state.timers.slice()
+    newTimers.push(timer)
+    this.setState({ timers: newTimers })
+  }
+
   render () {
+    const { timers } = this.state
+    window.localStorage.setItem('timers', timers)
+    console.log(timers)
+
     return (
-      <div>Hi</div>
+      <>
+        <h1>Timers</h1>
+        <p>Your <a href='https://en.wikipedia.org/wiki/UTC_offset'>UTC offset</a> is <strong>({toUTCString(UTC)})</strong>.</p>
+        <table className={styles.timers}>
+          <tbody>
+            {timers.map((timer, index) => {
+              const resetInfo = timer.getResetInfo()
+
+              switch (timer.constructor.name) {
+                case 'GCTimer':
+                  return (
+                    <tr key={index}>
+                      <td>{timer.constructor.timerName}</td>
+                      <td>{resetInfo.timeUntilNextReset.hours()}:{paddedZero(resetInfo.timeUntilNextReset.minutes())} Remaining ({resetInfo.nextReset.utcOffset(UTC).format('ddd M/D HH:mm')})</td>
+                    </tr>
+                  )
+              }
+            })}
+          </tbody>
+        </table>
+        <hr />
+        <AddTimerForm onAddTimer={this.handleOnAddTimer} />
+      </>
     )
   }
+}
+
+class AddTimerForm extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      timerType: TIMER_TYPES[0].name
+    }
+
+    this.handleOnAddTimer = this.handleOnAddTimer.bind(this)
+  }
+
+  handleOnAddTimer () {
+    const { onAddTimer } = this.props
+
+    switch (this.state.timerType) {
+      case 'GCTimer':
+        return onAddTimer(new GCTimer())
+      default:
+        console.log('UHOH')
+    }
+  }
+
+  render () {
+    return (
+      <form className={cn(zf.gridX, zf.gridPaddingX)}>
+        <div className={cn(zf.cell, zf.small12)}>
+          <select>
+            {TIMER_TYPES.map((timerType, index) =>
+              <option key={timerType.name} value={timerType.name}>{timerType.timerName}</option>
+            )}
+          </select>
+        </div>
+        <div className={cn(zf.cell, zf.small12)}>
+          <button type='button' className={zf.button} onClick={this.handleOnAddTimer}>+ Add Timer</button>
+        </div>
+      </form>
+    )
+  }
+}
+AddTimerForm.propTypes = {
+  onAddTimer: propTypes.func.isRequired
 }
 
 export default Timers
