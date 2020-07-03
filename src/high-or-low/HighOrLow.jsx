0bd0b2898051a8116cb2362aa1223cec4efd89df
Markus Tran <thebluepillow@gmail.com>
2020-07-03 16:34:43 -0700

    add high-low explanation

--- src/high-or-low/HighOrLow.jsx
+++ src/high-or-low/HighOrLow.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import cn from 'classnames'
 
+import MathJax from '../MathJax.jsx'
 import zf from '../foundation.scss'
 import styles from './HighOrLow.scss'
 
@@ -103,6 +104,20 @@ class HighOrLow extends Component {
               <><strong>Error</strong><p>Cannot have two of the same number.</p></>}
           </div>
         </div>
+        <br />
+
+        <h3>About</h3>
+        <p>The deck contains 9 cards valued 1 through 9. You and Tista-Bie are dealt 3 cards each, with 2 of hers and 1 of yours revealed. You guess if the sum of your 3 cards is higher or lower than the sum of her 3 cards. You win if you are correct.</p>
+        <p>There is a simple way to determine if your sum is more likely to be higher or lower. Calculate the <strong>scores</strong> of you and Tista-Bie according to the following formula:</p>
+        <MathJax $$={`
+          \\begin{align}
+            \\text{Your Score} & = (\\text{Your Card}) \\times 5 + 45, \\\\
+            \\text{TB’s Score} & = (\\text{TB’s Cards}) \\times 7.
+          \\end{align}`}
+        />
+        <p>Then make your guess based on if your score is higher or lower than Tista-Bie’s score.</p>
+        <p>For example, if your card is <MathJax $='7' />, then your score is <MathJax $='7 \times 5 + 45 = 80' />.<br />If Tista-Bie’s cards are <MathJax $='4' /> and <MathJax $='8' />, then her score is <MathJax $='(4 + 8) \times 7 = 84' />.</p>
+        <p>In this scenario, you should guess <b>Low</b>.</p>
       </>
     )
   }
