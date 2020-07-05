4c23a75aad15c690d1fb636978f00edd98bfabf7
Markus Tran <thebluepillow@gmail.com>
2020-07-04 23:26:40 -0700

    404 page lulu

--- src/404.jsx
+++ src/404.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import lulu404 from './lulu404.png'
 import zf from './foundation.scss'
+import styles from './404.scss'
 
 class _404 extends Component {
   componentDidMount () {
@@ -14,6 +16,10 @@ class _404 extends Component {
         <h1>404</h1>
         <p>This page does not exist.</p>
         <Link to='/' className={zf.button}>Go back home?</Link>
+        <div className={styles.lulu}>
+          <img src={lulu404} />
+          <p>How did you get here?</p>
+        </div>
       </>
     )
   }
