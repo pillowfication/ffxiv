358741cd57bebc762aa11f8293905c426184e51f
Your Name <you@example.com>
2020-07-07 00:47:01 -0700

    weather table

--- src/weather/Weather.jsx
+++ src/weather/Weather.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
 import cn from 'classnames'
+import EorzeaWeather from 'eorzea-weather'
 
+import WeatherIcon from './WeatherIcon.jsx'
+import calculateWeathers from './calculate-weathers'
+import * as zones from './zones'
 import zf from '../foundation.scss'
 import styles from './Weather.scss'
+
+const REGIONS = [
+  zones.LA_NOSCEA,
+  zones.THE_BLACK_SHROUD,
+  zones.THANALAN,
+  zones.ISHGARD_AND_SURROUNDING_AREAS,
+  zones.GYR_ABANIA,
+  zones.THE_FAR_EAST,
+  zones.NORVRANDT,
+  zones.OTHERS,
+  zones.EUREKA
+]
+const ZONES = REGIONS.map(region => region.zones).flat()
+const WEATHERS_COUNT = 5
 
 class Weather extends Component {
   constructor (props) {
@@ -14,10 +32,37 @@ class Weather extends Component {
   }
 
   render () {
+    const weathers = calculateWeathers(ZONES, WEATHERS_COUNT)
+    console.log(weathers)
+
     return (
       <>
         <h1>Weather</h1>
-        <p>hello</p>
+        <div className={zf.tableScroll}>
+          <table className={styles.weathers}>
+            <tbody>
+              {REGIONS.map(region =>
+                <React.Fragment key={region.name}>
+                  <tr className={styles.region}>
+                    <th colSpan={WEATHERS_COUNT + 2}>{region.name}</th>
+                  </tr>
+                  {region.zones.map(zone =>
+                    <tr key={zone}>
+                      <th className={styles.zone}>{(new EorzeaWeather(zone)).getZoneName()}</th>
+                      {weathers[zone].map((weather, index) =>
+                        <td key={index}>
+                          <WeatherIcon name={weather} />
+                          <br />
+                          {weather}
+                        </td>
+                      )}
+                    </tr>
+                  )}
+                </React.Fragment>
+              )}
+            </tbody>
+          </table>
+        </div>
       </>
     )
   }
---
+++ src/weather/zones.js
@@ -0,0 +1,107 @@
+import EorzeaWeather from 'eorzea-weather'
+
+export const LA_NOSCEA = {
+  name: 'La Noscea',
+  zones: [
+    EorzeaWeather.ZONE_LIMSA_LOMINSA,
+    EorzeaWeather.ZONE_MIDDLE_LA_NOSCEA,
+    EorzeaWeather.ZONE_LOWER_LA_NOSCEA,
+    EorzeaWeather.ZONE_EASTERN_LA_NOSCEA,
+    EorzeaWeather.ZONE_WESTERN_LA_NOSCEA,
+    EorzeaWeather.ZONE_UPPER_LA_NOSCEA,
+    EorzeaWeather.ZONE_OUTER_LA_NOSCEA
+  ]
+}
+
+export const THE_BLACK_SHROUD = {
+  name: 'The Black Shroud',
+  zones: [
+    EorzeaWeather.ZONE_GRIDANIA,
+    EorzeaWeather.ZONE_CENTRAL_SHROUD,
+    EorzeaWeather.ZONE_EAST_SHROUD,
+    EorzeaWeather.ZONE_SOUTH_SHROUD,
+    EorzeaWeather.ZONE_NORTH_SHROUD
+  ]
+}
+
+export const THANALAN = {
+  name: 'Thanalan',
+  zones: [
+    EorzeaWeather.ZONE_ULDAH,
+    EorzeaWeather.ZONE_WESTERN_THANALAN,
+    EorzeaWeather.ZONE_CENTRAL_THANALAN,
+    EorzeaWeather.ZONE_EASTERN_THANALAN,
+    EorzeaWeather.ZONE_SOUTHERN_THANALAN,
+    EorzeaWeather.ZONE_NORTHERN_THANALAN
+  ]
+}
+
+export const ISHGARD_AND_SURROUNDING_AREAS = {
+  name: 'Ishgard and Surrounding Areas',
+  zones: [
+    EorzeaWeather.ZONE_ISHGARD,
+    EorzeaWeather.ZONE_COERTHAS_CENTRAL_HIGHLANDS,
+    EorzeaWeather.ZONE_COERTHAS_WESTERN_HIGHLANDS,
+    EorzeaWeather.ZONE_THE_SEA_OF_CLOUDS,
+    EorzeaWeather.ZONE_AZYS_LLA,
+    EorzeaWeather.ZONE_IDYLLSHIRE,
+    EorzeaWeather.ZONE_THE_DRAVANIAN_FORELANDS,
+    EorzeaWeather.ZONE_THE_DRAVANIAN_HINTERLANDS,
+    EorzeaWeather.ZONE_THE_CHURNING_MISTS
+  ]
+}
+
+export const GYR_ABANIA = {
+  name: 'Gyr Abania',
+  zones: [
+    EorzeaWeather.ZONE_RHALGRS_REACH,
+    EorzeaWeather.ZONE_THE_FRINGES,
+    EorzeaWeather.ZONE_THE_PEAKS,
+    EorzeaWeather.ZONE_THE_LOCHS
+  ]
+}
+
+export const THE_FAR_EAST = {
+  name: 'The Far East',
+  zones: [
+    EorzeaWeather.ZONE_KUGANE,
+    EorzeaWeather.ZONE_THE_RUBY_SEA,
+    EorzeaWeather.ZONE_YANXIA,
+    EorzeaWeather.ZONE_THE_AZIM_STEPPE
+  ]
+}
+
+export const NORVRANDT = {
+  name: 'Norvrandt',
+  zones: [
+    EorzeaWeather.ZONE_THE_CRYSTARIUM,
+    EorzeaWeather.ZONE_EULMORE,
+    EorzeaWeather.ZONE_LAKELAND,
+    EorzeaWeather.ZONE_KHOLUSIA,
+    EorzeaWeather.ZONE_AMH_ARAENG,
+    EorzeaWeather.ZONE_IL_MHEG,
+    EorzeaWeather.ZONE_THE_RAKTIKA_GREATWOOD,
+    EorzeaWeather.ZONE_THE_TEMPEST
+  ]
+}
+
+export const EUREKA = {
+  name: 'Eureka',
+  zones: [
+    EorzeaWeather.ZONE_EUREKA_ANEMOS,
+    EorzeaWeather.ZONE_EUREKA_PAGOS,
+    EorzeaWeather.ZONE_EUREKA_PYROS,
+    EorzeaWeather.ZONE_EUREKA_HYDATOS
+  ]
+}
+
+export const OTHERS = {
+  name: 'Others',
+  zones: [
+    EorzeaWeather.ZONE_MOR_DHONA,
+    EorzeaWeather.ZONE_MIST,
+    EorzeaWeather.ZONE_THE_LAVENDER_BEDS,
+    EorzeaWeather.ZONE_THE_GOBLET,
+    EorzeaWeather.ZONE_SHIROGANE
+  ]
+}
