01fef20451acb55e370f329631098c2d6ab19504
Markus Tran <thebluepillow@gmail.com>
2020-07-11 21:19:22 -0700

    add high or low chances

--- modules/high-or-low/HighOrLowCalculator.js
+++ modules/high-or-low/HighOrLowCalculator.js
@@ -5,6 +5,10 @@ import Button from '@material-ui/core/Button'
 import HighOrLowCard from './HighOrLowCard'
 import calculateHighOrLow from './calculate-high-or-low'
 
+function toPercent (p, q) {
+  return (p / q * 100 | 0) + '%'
+}
+
 const useStyles = makeStyles((theme) => ({
   cardsContainer: {
     textAlign: 'center',
@@ -18,7 +22,15 @@ export default function HighOrLowCalculator (props) {
   const [tb2, setTb2] = useState(null)
   const [me, setMe] = useState(null)
   const classes = useStyles()
-  const { err, tb1Err, tb2Err, meErr, guess } = calculateHighOrLow(tb1, tb2, me)
+  const {
+    err,
+    tb1Err,
+    tb2Err,
+    meErr,
+    high,
+    low,
+    both
+  } = calculateHighOrLow(tb1, tb2, me)
 
   const handleClickReset = (event) => {
     setTb1(null)
@@ -43,15 +55,16 @@ export default function HighOrLowCalculator (props) {
               return <Typography paragraph>Input cards above</Typography>
             case 'DUPLICATE':
               return <Typography paragraph>Cannot have duplicate cards</Typography>
-            default:
-              switch (guess) {
-                case 'LOW':
-                  return <Typography paragraph>You are <b>Low</b></Typography>
-                case 'BOTH':
-                  return <Typography paragraph>You are <b>High</b> or <b>Low</b></Typography>
-                case 'HIGH':
-                  return <Typography paragraph>You are <b>High</b></Typography>
+            default: {
+              const sum = high + low + both
+              if (high > low) {
+                return <Typography paragraph>You are <b>High</b> ({toPercent(high, sum)})</Typography>
+              } else if (high < low) {
+                return <Typography paragraph>You are <b>Low</b> ({toPercent(low, sum)})</Typography>
+              } else {
+                return <Typography paragraph>You are <b>High</b> or <b>Low</b> (50%)</Typography>
               }
+            }
           }
         })()}
         <Button variant='contained' color='secondary' onClick={handleClickReset}>Reset</Button>
--- modules/high-or-low/calculate-high-or-low.js
+++ modules/high-or-low/calculate-high-or-low.js
@@ -1,3 +1,11 @@
+function choose2 (arr) {
+  if (arr.length === 2) {
+    return [arr[0] + arr[1]]
+  }
+  const slice = arr.slice(1)
+  return slice.map((x) => x + arr[0]).concat(choose2(slice))
+}
+
 export default function calculateHighOrLow (tb1, tb2, me) {
   const tb1Err = tb1 && (tb1 === tb2 || tb1 === me)
   const tb2Err = tb2 && (tb2 === tb1 || tb2 === me)
@@ -17,10 +25,30 @@ export default function calculateHighOrLow (tb1, tb2, me) {
     }
   }
 
-  const tbEv = 7 * (tb1 + tb2)
-  const meEv = 5 * me + 45
+  const hiddenCards = []
+  Array(9).fill().forEach((_, index) => {
+    const card = index + 1
+    if (card !== tb1 && card !== tb2 && card !== me) {
+      hiddenCards.push(card)
+    }
+  })
+
+  const tbPossibles = hiddenCards.map((card) => tb1 + tb2 + card)
+  const mePossibles = choose2(hiddenCards).map((sum) => me + sum)
+
+  let high = 0
+  let low = 0
+  let both = 0
+  for (const me of mePossibles) {
+    for (const tb of tbPossibles) {
+      me > tb ? ++high : me < tb ? ++low : ++both
+    }
+  }
+
   return {
     err: null,
-    guess: meEv > tbEv ? 'HIGH' : meEv < tbEv ? 'LOW' : 'BOTH'
+    high,
+    low,
+    both
   }
 }
