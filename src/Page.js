4bffa92e14ba1001c8e68b820d008827c20ed7a1
Markus Tran <thebluepillow@gmail.com>
2020-07-26 05:35:21 -0700

    Page titles

--- src/Page.js
+++ src/Page.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Head from 'next/head'
 import Typography from '@material-ui/core/Typography'
 
 const Page = ({ title, children }) => {
   return (
     <>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <Typography variant='h1' gutterBottom>{title}</Typography>
       {children}
     </>
