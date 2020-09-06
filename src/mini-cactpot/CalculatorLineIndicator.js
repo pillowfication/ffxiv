2f2583a031a2edac8495b5325f07766de155b060
Markus Tran <thebluepillow@gmail.com>
2020-09-05 17:54:18 -0700

    add line indicators

---
+++ src/mini-cactpot/CalculatorLineIndicator.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(1),
+    fontSize: '2em',
+    transform: ({ rotate }) => `rotate(${rotate}deg)`
+  }
+}))
+
+const CalculatorLineIndicator = ({ rotate, suggested }) => {
+  const classes = useStyles({ rotate })
+
+  return (
+    <ArrowForwardIcon
+      className={classes.root}
+      color={suggested ? 'primary' : 'disabled'}
+    />
+  )
+}
+
+CalculatorLineIndicator.propTypes = {
+  rotate: PropTypes.number.isRequired,
+  suggested: PropTypes.bool
+}
+
+export default CalculatorLineIndicator
