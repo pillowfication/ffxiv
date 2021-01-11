a1c0ba428d84b1a6126d9deab90244a85d2ed256
Markus Tran <thebluepillow@gmail.com>
2021-01-11 13:46:53 -0800

    generate-roegadyn

--- pages/name-generator.tsx
+++ pages/name-generator.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Page from '../src/Page'
-import generateLalafell from '../src/name-generator/generate-lalafell'
+import generateRoegadyn from '../src/name-generator/generate-roegadyn'
 import { Clan, Gender } from '../src/name-generator/types'
 
 const NameGenerator = () => {
   return (
     <Page title='Name Generator'>
       <Typography paragraph>
-        {Array(10).fill(null).map(() => generateLalafell(Clan.Dunesfolk, Gender.Male)).join(', ')}
+        {Array(10).fill(null).map(() => generateRoegadyn(Clan.Hellsguard, Gender.Female)).join(', ')}
       </Typography>
     </Page>
   )
---
+++ src/name-generator/generate-roegadyn.ts
@@ -0,0 +1,28 @@
+import data from './data/roegadyn.json'
+import { Clan, Gender } from './types'
+import { randomElement } from './utils'
+
+const FORENAMES = {
+  [Clan.SeaWolves]: {
+    [Gender.Male]: data.sea_wolves.male_forenames,
+    [Gender.Female]: data.sea_wolves.female_forenames
+  },
+  [Clan.Hellsguard]: data.hellsguard.forenames
+}
+
+const SURNAMES = {
+  [Clan.SeaWolves]: {
+    [Gender.Male]: data.sea_wolves.male_surnames,
+    [Gender.Female]: data.sea_wolves.female_surnames
+  },
+  [Clan.Hellsguard]: {
+    [Gender.Male]: data.hellsguard.male_surnames,
+    [Gender.Female]: data.hellsguard.female_surnames
+  }
+}
+
+export default function generateRoegadyn (clan: Clan.SeaWolves | Clan.Hellsguard, gender: Gender) {
+  const forename = randomElement(clan === Clan.SeaWolves ? FORENAMES[clan][gender] : FORENAMES[clan])
+  const surname = randomElement(SURNAMES[clan][gender])
+  return `${forename} ${surname}`
+}
